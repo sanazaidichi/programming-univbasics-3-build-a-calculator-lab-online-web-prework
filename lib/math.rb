0a8da5385a837210ab012d9ede4577fb82c2e464
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-10-28 08:39:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
-def addition(num1, num2)
-
+def addition num1, num2 
+  p num1 + num2
 end
 
-def subtraction(num1, num2)
-
+def subtraction num1, num2 
+  p num2 - num1
 end
 
-def division(num1, num2)
-
+def multiplication num1, num2 
+  p num2*num1
 end
 
-def multiplication(num1, num2)
-
+def divisiion num1, num2 
+  p num2 / num1
 end
 
-def modulo(num1, num2)
+def modulo num1, num2 
+  p modulo num1 % num2
+  p % remainder
+end 
 
-end
-
-def square_root(num)
-
+def square_root num1 
+  p Math.sqrt(num1)
 end
